c4fb5e275f4de90bb77e5122fefd7894e6794486
therandomfool <mddenver@protonmail.com>
2021-02-26 16:25:42 -0800

    Added hold class

--- app.js
+++ app.js
@@ -1,84 +1,133 @@
+let pos = [];
+
+
+let pos1 = document.getElementById('c1').innerHTML = 0;
+let pos2 = document.getElementById('c2').innerHTML = 0;
+let pos3 = document.getElementById('c3').innerHTML = 0;
+let pos4 = document.getElementById('c4').innerHTML = 0;
+let pos5 = document.getElementById('c5').innerHTML = 0;
+let pos6 = document.getElementById('c6').innerHTML = 0;
+let pos7 = document.getElementById('c7').innerHTML = 0;
+let pos8 = document.getElementById('c8').innerHTML = 0;
+let pos9 = document.getElementById('c9').innerHTML = 0;
+
+function generate() {
+    getRandomNumber();
 
 
 
-
-function generate(){
-    
-    function shuffle1(array) {
-        var i = array.length,
-        j = 0,
-        temp;
-    
-        while (i--) {
-    
-            j = Math.floor(Math.random() * (i+1));
-    
-            // swap randomly chosen element with current element
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-            
-            let pos1 = document.getElementById('c1').innerHTML = array[0];
-            let pos2 = document.getElementById('c2').innerHTML = array[1];
-           let pos3 = document.getElementById('c3').innerHTML = array[2];
-           
-          
-        }
-    
-        return array;
-    }
-    function shuffle2(array) {
-        var i = array.length,
-            j = 0,
-            temp;
-            
-        while (i--) {
-            
-            j = Math.floor(Math.random() * (i+1));
-            
-            // swap randomly chosen element with current element
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-    
-            
-           let pos4 = document.getElementById('c4').innerHTML = array[3];
-           let pos5 = document.getElementById('c5').innerHTML = array[4];
-           let pos6 = document.getElementById('c6').innerHTML = array[5];
-            
-        }
-    
-        return array;
-    }
-    function shuffle3(array) {
-        var i = array.length,
-        j = 0,
-            temp;
-    
-            while (i--) {
-                
-                j = Math.floor(Math.random() * (i+1));
-                
-            // swap randomly chosen element with current element
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-    
-            
-            let pos7 = document.getElementById('c7').innerHTML = array[6];
-            let pos8 = document.getElementById('c8').innerHTML = array[7];
-            let pos9 = document.getElementById('c9').innerHTML = array[8];
-        }
-        
-        return array;
-    }
-    
-    var ranNums = shuffle1([1,2,3,4,5,6,7,8,9]);
-    var ranNums = shuffle2([1,2,3,4,5,6,7,8,9]);
-    var ranNums = shuffle3([1,2,3,4,5,6,7,8,9]);
 }
 
+function getRandomNumber() {
+    for (let i = 1; i < 10; i++) {
 
-function match(){
+        // if(document.getElementsByClassName != 'hold'){
+        //     pos[i] = Math.floor(Math.random() * 9);
+        //     pos[i] = Math.floor(Math.random() * 9);
 
+        // }
+
+
+        let temp;
+        switch (i) {
+            case 1:
+                let temp1 = document.getElementById('c1')
+                if (temp1.classList.contains('hold')) {
+                    pos1 = document.getElementById('c1').innerHTML = pos[1];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos1 = document.getElementById('c1').innerHTML = pos[1];
+                }
+                break;
+            case 2:
+                let temp2 = document.getElementById('c2')
+                if (temp2.classList.contains('hold')) {
+                    pos2 = document.getElementById('c2').innerHTML = pos[2];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos2 = document.getElementById('c2').innerHTML = pos[2];
+                }
+                break;
+            case 3:
+                let temp3 = document.getElementById('c3')
+                if (temp3.classList.contains('hold')) {
+                    pos3 = document.getElementById('c3').innerHTML = pos[3];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos3 = document.getElementById('c3').innerHTML = pos[3];
+                }
+                break;
+            case 4:
+                let temp4 = document.getElementById('c4')
+                if (temp4.classList.contains('hold')) {
+                    pos4 = document.getElementById('c4').innerHTML = pos[4];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos4 = document.getElementById('c4').innerHTML = pos[4];
+                }
+                break;
+            case 5:
+                let temp5 = document.getElementById('c5')
+                if (temp5.classList.contains('hold')) {
+                    pos5 = document.getElementById('c5').innerHTML = pos[5];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos5 = document.getElementById('c5').innerHTML = pos[5];
+                }
+                break;
+            case 6:
+                let temp6 = document.getElementById('c6')
+                if (temp6.classList.contains('hold')) {
+                    pos6 = document.getElementById('c6').innerHTML = pos[6];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos6 = document.getElementById('c6').innerHTML = pos[6];
+                }
+                break;
+            case 7:
+                let temp7 = document.getElementById('c7')
+                if (temp7.classList.contains('hold')) {
+                    pos7 = document.getElementById('c7').innerHTML = pos[7];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos7 = document.getElementById('c7').innerHTML = pos[7];
+                }
+                break;
+            case 8:
+                let temp8 = document.getElementById('c8')
+                if (temp8.classList.contains('hold')) {
+                    pos8 = document.getElementById('c8').innerHTML = pos[8];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos8 = document.getElementById('c8').innerHTML = pos[8];
+                }
+                break;
+            case 9:
+                let temp9 = document.getElementById('c9')
+                if (temp9.classList.contains('hold')) {
+                    pos9 = document.getElementById('c9').innerHTML = pos[9];
+                } else {
+                    pos[i] = Math.floor(Math.random() * 9+1);
+                    pos9 = document.getElementById('c9').innerHTML = pos[9];
+                }
+                break;
+
+        }
+
+
+    }
+}
+
+function holdNumber(clicked_id) {
+    let element = document.getElementById(clicked_id);
+
+    if (document.getElementById(clicked_id).style.background == "gold") {
+        document.getElementById(clicked_id).style.background = "white"
+        element.classList.toggle('hold')
+    } else {
+        document.getElementById(clicked_id).style.background = "gold"
+        element.classList.toggle('hold')
+    }
+
+    ;
 }
